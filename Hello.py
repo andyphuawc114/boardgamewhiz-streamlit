890757d2955e0fdfd939d8e5aca6d16b404ea262
Wei Chuen Andy Phua <144690930+andyphuawc114@users.noreply.github.com>
2023-10-28 10:59:57 +0000

    Make new pages, connect to bigquery fore reviews

--- Hello.py
+++ Hello.py
@@ -18,84 +18,102 @@ from st_files_connection import FilesConnection
 from st_pages import Page, show_pages, add_page_title
 import pandas as pd
 import plotly.express as px
+import numpy as np
+
+st.set_page_config(
+    page_title="BoardGameWhiz",
+    page_icon="👋",
+    layout="wide"
+)
+
 
 LOGGER = get_logger(__name__)
 
+@st.cache_resource
+def get_data():
+    conn = st.experimental_connection('gcs', type=FilesConnection)
+    df = conn.read("boardgamewhiz-bucket/boardgames_cleaned.csv", input_format="csv", ttl=600)
+    return df
 
-def run():
-    st.set_page_config(
-        page_title="BoardGameWhiz",
-        page_icon="👋",
-        layout="wide"
-    )
 
-    # load data
-    #conn = st.experimental_connection('gcs', type=FilesConnection)
-    #df = conn.read("boardgamewhiz-bucket/boardgames.csv", input_format="csv", ttl=600)
+df = get_data()
 
-    st.write('''# BoardGameWhiz''')
-    
-    st.write(":balloon: *Welcome to BoardGameWhiz - Translating Board Game Data into Insights!* 👋")
+if 'main_data' not in st.session_state:
+    st.session_state['main_data'] = df
 
-    # rename the sidebar menu labelling
-    show_pages(
-    [
-        Page("Hello.py", "Home"),
-        Page('pages/0_Trends.py', "Trend Visualizer"),
-        Page('pages/0_Animation_Demo.py', "Animation"),
-        Page('pages/1_Plotting_Demo.py', "Plot"),
-        Page('pages/2_Mapping_Demo.py', "Map"),
-        Page('pages/3_DataFrame_Demo.py', "DataFrame"),        
-    ]
-    ) 
-    
-    #st.sidebar.success("Select a demo above.")
+# load data
+#conn = st.experimental_connection('gcs', type=FilesConnection)
+#df = conn.read("boardgamewhiz-bucket/boardgames.csv", input_format="csv", ttl=600)
 
-    st.markdown(
-        """
-        ### Introduction
-        BoardGameWhiz is a user-centric board game visualizer built for the board game community.
-        The data source is based on [boardgamegeek](https://boardgamegeek.com/)
+st.write('''# BoardGameWhiz''')
 
-        **👈 Select a demo from the sidebar** to navigage to other functions.
+st.write(":balloon: *Welcome to BoardGameWhiz - Translating Board Game Data into Insights!* 👋")
+
+# rename the sidebar menu labelling
+show_pages(
+[
+    Page("Hello.py", "Home"),
+    Page('pages/0_Trends.py', "Recommendation"),
+    Page('pages/0_Animation_Demo.py', "Animation"),
+    Page('pages/1_Plotting_Demo.py', "Plot"),
+    Page('pages/2_Mapping_Demo.py', "Map"),
+    Page('pages/3_DataFrame_Demo.py', "DataFrame"),        
+]
+) 
+
+#st.sidebar.success("Select a demo above.")
+
+st.markdown(
     """
-    )
+    ### Introduction
+    BoardGameWhiz is a user-centric board game visualizer built for the board game community.
+    The data source is based on [boardgamegeek](https://boardgamegeek.com/)
 
-    @st.cache_data
-    def get_data():
-        conn = st.experimental_connection('gcs', type=FilesConnection)
-        df = conn.read("boardgamewhiz-bucket/boardgames_cleaned.csv", input_format="csv", ttl=600)
-        return df
+    **👈 Select a demo from the sidebar** to navigage to other functions.
+"""
+)
 
-    df = get_data()
-
-    row3_space1, row3_1, row3_space2, row3_2, row3_space3 = st.columns(
-    (0.1, 1, 0.1, 1, 0.1))
-
-    with row3_1:
-        st.subheader("Average User Ratings Trend")
-        df_ratings = df[['bgg_id', 'name', 'year', 'avg_rating']]
-        df_ratings = df_ratings[(df_ratings['year'] >= 2000) & (df_ratings['year'] <= 2023)]
-        df_ratings_avg = df_ratings.groupby('year')['avg_rating'].mean().reset_index()
-
-        fig = px.line(df_ratings_avg, x="year", y="avg_rating",
-              labels={"year": "Year Published","avg_rating": "Avg User Rating"})
-        
-        fig.show()
-
-        st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-
-    with row3_2:
-        st.subheader("Testing")
-
-        fig = px.line(df_ratings_avg, x="year", y="avg_rating",
-              labels={"year": "Year Published","avg_rating": "Avg User Rating"})
-        
-        fig.show()
-
-        st.plotly_chart(fig, theme="streamlit", use_container_width=True)        
+row3_space1, row3_1, row3_space2, row3_2, row3_space3 = st.columns(
+(0.1, 1, 0.1, 1, 0.1))
 
 
+# LINE CHART TO SHOW USER RATINGS TREND
+with row3_1:
+    st.subheader("Average User Ratings Trend")
+    df_ratings = df[['bgg_id', 'name', 'year', 'avg_rating']]
+    df_ratings = df_ratings[(df_ratings['year'] >= 2000) & (df_ratings['year'] <= 2023)]
+    df_ratings_avg = df_ratings.groupby('year')['avg_rating'].mean().reset_index()
 
-if __name__ == "__main__":
-    run()
+    fig = px.line(df_ratings_avg, x="year", y="avg_rating",
+            labels={"year": "Year Published","avg_rating": "Avg User Rating"})
+
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+
+# STACKED BAR CHART TO SHOW USER RATING BY GENRE
+with row3_2:
+    st.subheader("User Rating by Genre")
+
+    df_genre = df[['bgg_id', 'name', 'year', 'avg_rating', 'abstracts', 'cgs', 'childrensgames', 'familygames', 'partygames', 'strategygames', 'thematic', 'wargames']]
+    df_genre['avg_rating_group'] = df_genre['avg_rating'].apply(np.floor)
+    df_genre = df_genre[df_genre['avg_rating'] > 0.00]
+    df_genre = df_genre[['avg_rating_group','abstracts', 'cgs', 'childrensgames', 'familygames', 'partygames', 'strategygames', 'thematic', 'wargames']]
+    genre_dict = {'abstracts': 'Abstract', 'cgs': 'Customizable', 'childrensgames': 'Children', 'familygames': 'Family', 'partygames': 'Party',
+            'strategygames': 'Strategy', 'thematic': 'Thematic', 'wargames': 'War'}
+    df_genre = df_genre.rename(genre_dict, axis = 1)
+    df_genre_rating = df_genre.groupby('avg_rating_group').sum().reset_index()
+    df_genre_rating = pd.melt(df_genre_rating, id_vars=['avg_rating_group'])
+    fig = px.bar(df_genre_rating, x="avg_rating_group", y="value", color="variable",
+        labels={"avg_rating_group": "User Rating","value": "Game Count", "variable": "Genre"})
+
+    fig.update_layout(legend=dict(
+        yanchor="top",
+        y=0.99,
+        xanchor="left",
+        x=0.01
+    ))
+
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)        
+
+# if __name__ == "__main__":
+#     run(df)
--- pages/0_Trends.py
+++ pages/0_Trends.py
@@ -1,32 +1,56 @@
 import pandas as pd
 import streamlit as st
-from st_files_connection import FilesConnection
+from google.oauth2 import service_account
 
-
-def show_table(df):
-    selected_df = df.iloc[0:10]
-    selected_df['year'] = selected_df['year'].astype('str')
-    selected_df = selected_df[['rank', 'bgg_id','name','year']]
-
-    st.dataframe(selected_df, hide_index=True)
-
-def trend_visual():
-    @st.cache_data
-    def get_data():
-        conn = st.experimental_connection('gcs', type=FilesConnection)
-        df = conn.read("boardgamewhiz-bucket/boardgames.csv", input_format="csv", ttl=600)
-        return df
-
-    df = get_data()
-    st.write("Top 10 Board Games")
-
-    show_table(df)
-
-st.set_page_config(page_title="Trends Visualizer", page_icon="📊")
-st.markdown("# Trends")
+st.set_page_config(page_title="Recommendation",
+                   page_icon="📊",
+                   layout = 'wide')
+st.markdown("# Board Game Recommendation")
 st.write(
     """This page is a trend visualizer to shows the trends and characteristics
     of various board games"""
 )
 
-trend_visual()
+
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+
+query  = '''
+SELECT  * FROM `tensile-walker-401308.eng_reviews.reviews` 
+where bgg_id = 224517
+LIMIT 10;
+'''
+new_df = pd.read_gbq(query, credentials = credentials)
+
+st.dataframe(new_df, hide_index=True)
+
+df = st.session_state['main_data']
+
+st.write("Top 10 Board Games")
+
+selected_df = df.iloc[0:10]
+selected_df['year'] = selected_df['year'].astype('str')
+selected_df = selected_df[['rank', 'bgg_id','name','year','thumbnail']]
+#selected_df['link'] = selected_df['bgg_id'].apply(lambda x: "https://boardgamegeek.com/boardgame/" + str(x))
+
+# Converting links to html tags
+def path_to_image_html(path):
+    return '<img src="' + path + '" width="60" >'
+
+def path_to_url_html(bgg_id):
+    return '<a href="https://boardgamegeek.com/boardgame/' + str(bgg_id) + '">' + str(bgg_id) + "<" + "/" "a>"
+
+@st.cache_data
+def convert_df(input_df):
+     # IMPORTANT: Cache the conversion to prevent computation on every rerun
+     return input_df.to_html(escape=False, formatters=dict(thumbnail=path_to_image_html, bgg_id=path_to_url_html))
+
+html = convert_df(selected_df)
+
+st.markdown(
+    html,
+    unsafe_allow_html=True
+)
+
+# st.dataframe(selected_df, hide_index=True)
